0f0ae060f91b60a0194aba109a79b9d032594c98
Caio <caio.rodrigues.barbosa@usp.br>
2020-10-26 18:42:01 -0300

    inserindo mais dados no banco

--- projeto_banco_de_dados/index.sql
+++ projeto_banco_de_dados/index.sql
@@ -11,7 +11,11 @@ CREATE TABLE projeto_final.produto (
 );
 
 INSERT INTO projeto_final.produto (nome, descricao, preco, nome_imagem)
-VALUES ('Geladeira BRASTEMP', 'Refrigere sua comida com alta qualidade!', 2512.25, 'caminho/da/imagem');
+VALUES ('Geladeira Dako', 'Refrigere sua comida com alta qualidade!', 2512.25, 'caminho/da/imagem'),
+('Geladeira Brastemp INOX', 'Geladeira Frost Free Brastemp Side Inverse 540 litros', 5019.00, './imagens/geladeira_brastemp.jpeg'),
+('Geladeira Brastemp Branca', 'Geladeira Frost Free Brastemp Branca 375 litros', 1910.90, './imagens/refrigerador_brastemp.jpeg'),
+('Fogão Consul', 'Fogão 4 Bocas Consul Inox com Mesa de Vidro', 1129.00, './imagens/fogao_consul.jpeg'),
+('Lavadora Philco', 'Lavadora de Roupas Philco Inverter 12KG', 2399.90, './imagens/lavadora_philco.jpeg');
 
 CREATE TABLE projeto_final.pedido (
   id_pedido INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -25,5 +29,8 @@ CREATE TABLE projeto_final.pedido (
 );
 
 INSERT INTO projeto_final.pedido (nome_cliente, endereco, telefone, nome_produto, valor_unitario, quantidade, valor_total)
-VALUES ('João', 'Avenida da alegria', 1158585252, 'Geladeira BRASTEMP', 2512.25, 2, 5024.50);
-;
+VALUES ('João', 'Avenida da alegria', 1158585252, 'Geladeira BRASTEMP Branca', 1910.90, 1, 1910.90),
+('José', 'Rua do Açai', 115800002, 'Geladeira BRASTEMP INOX', 2399.90, 1, 2399.90),
+('Maria', 'Avenida Professor', 4444555511, 'Fogão Consul', 1129.00, 1, 1129.00),
+('Jeck', 'Rua quatro', 5555555544, 'Lavadora Philco', 2399.90, 1, 2399.90),
+('Guilherme', 'Avenida Monteiro', 2222444488, 'Geladeira Dako', 2512.25, 1, 2512.25);
